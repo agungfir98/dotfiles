d0f9bb92b272e129bde0198fe99334cf454af25d
agungfir98 <agungfir98@gmail.com>
2024-12-14 18:16:36 +0700

    add more config

--- .config/nvim/lua/plugins/gruvboxTheme.lua
+++ .config/nvim/lua/plugins/gruvboxTheme.lua
@@ -4,6 +4,16 @@ return {
   config = function()
     require("gruvbox").setup({
       transparent_mode = true,
+      terminal_colors = true,
+      dim_inactive = false,
+      bold = false,
+      italic = {
+        comments = true,
+        operators = true,
+        strings = true,
+        folds = true,
+        emphasis = true,
+      },
     })
   end,
 }
